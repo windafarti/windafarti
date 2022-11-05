8491c3bd015b73931d682990bd8f703f9b2de679
windafarti <117498603+windafarti@users.noreply.github.com>
2022-11-05 11:16:34 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "jalpy.py",
+    version = "0.1",
+    py_modules = ['jalpy'],
+    url = "https://github.com/demotomohiro/remocolab",
+    author = "celle2",
+    install_requires = ["installCRD"]
+)
