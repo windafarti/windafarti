68991e2dd5375de6496573cf29bb3e730b0e971d
windafarti <117498603+windafarti@users.noreply.github.com>
2022-11-05 11:18:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name = "jalpy.py",
+    name = "jewer.py",
     version = "0.1",
-    py_modules = ['jalpy'],
-    url = "https://github.com/demotomohiro/remocolab",
-    author = "celle2",
-    install_requires = ["installCRD"]
+    py_modules = ['jewer'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "indraxz",
+    install_requires = ["pyngrok"]
 )
